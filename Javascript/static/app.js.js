10eeb9a727476ed435dee62995d3de3c9aa37c28
adrijm321 <adriannejmiller94@gmail.com>
2020-01-16 14:23:12 -0800

    committing changes

---
+++ Javascript/static/app.js.js
@@ -0,0 +1,8 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+// Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+
+console.log(data);
